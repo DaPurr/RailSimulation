79ab5e3021288aaf0960499db4f2ebe44ab631be
Nemanja Milovanovic <nemanja@live.nl>
2016-04-07 14:34:08 +0200

    Add TrainType enumeration.
    This enumeration is used to model the different train types.

---
+++ src/wagon/components/rollingstock/TrainType.java
@@ -0,0 +1,18 @@
+package wagon.components.rollingstock;
+
+/**
+ * This enumeration reflects the different type of rolling stock that is operated.
+ * 
+ * @author Nemanja Milovanovic
+ *
+ */
+
+public enum TrainType {
+	DDM,
+	DDZ,
+	DM90,
+	ICM,
+	SGM,
+	SLT,
+	VIRM 
+}
