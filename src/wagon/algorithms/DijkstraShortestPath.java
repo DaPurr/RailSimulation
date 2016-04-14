1e366720c1f9abf4da733dbaeaa139af990a3eb8
Nemanja Milovanovic <nemanja@live.nl>
2016-04-14 16:21:38 +0200

    Finish dijkstra's algorithm.

--- src/wagon/algorithms/DijkstraShortestPath.java
+++ src/wagon/algorithms/DijkstraShortestPath.java
@@ -2,10 +2,8 @@ package wagon.algorithms;
 
 import java.util.*;
 
-import org.jgrapht.util.FibonacciHeap;
-import org.jgrapht.util.FibonacciHeapNode;
-
 import wagon.network.Node;
+import wagon.network.WeightedEdge;
 import wagon.network.expanded.EventActivityNetwork;
 
 public class DijkstraShortestPath {
@@ -23,13 +21,75 @@ public class DijkstraShortestPath {
 	}
 	
 	public Path shortestPath(Node start, Node stop) {
-		// initialize algorithm
-		FibonacciHeap<Node> unvisited = new FibonacciHeap<>();
+		
+		Map<Node, Double> distance = new HashMap<>();
+		
+		// init queue
+		PriorityQueue<DijkstraNode<Node>> queue = new PriorityQueue<>();
+
+		// init distances
 		for (Node node : network.nodeSet()) {
-			FibonacciHeapNode<Node> heapNode = new FibonacciHeapNode<Node>(node);
+			distance.put(node, Double.POSITIVE_INFINITY);
+		}
+
+		// insert source
+		queue.add(new DijkstraNode<Node>(start, 0.0));
+		
+		while (!queue.isEmpty()) {
+			DijkstraNode<Node> dijkU = queue.poll();
+			if (dijkU.e == stop)
+				return constructPath(dijkU);
+			double oldLabel = distance.get(dijkU.e);
+			if (dijkU.weight < oldLabel) {
+				distance.put(dijkU.e, dijkU.weight);
+			}
+			
+			// traverse neighbors
+			Set<WeightedEdge> outEdges = network.outgoingEdges(dijkU.e);
+			for (WeightedEdge edge : outEdges) {
+				Node v = edge.target();
+				if (edge.source() != dijkU.e)
+					throw new IllegalStateException("Inconsistency detected");
+				if (dijkU.weight + edge.weight() < distance.get(v)) {
+					queue.add(new DijkstraNode<Node>(v, dijkU.weight + edge.weight()));
+					distance.put(v, dijkU.weight + edge.weight());
+				}
+			}
+		}
+		throw new IllegalStateException("Could not find sink node(s)");
+	}
+	
+	private Path constructPath(DijkstraNode<Node> end) {
+		DefaultPath path = new DefaultPath();
+		DijkstraNode<Node> nextNode = end;
+		while (nextNode != null) {
+			path.addEdge(network.getEdge(nextNode.previous.e, nextNode.e));
+			nextNode = nextNode.previous;
+		}
+		return path;
+	}
+	
+	private class DijkstraNode<E> implements Comparable<DijkstraNode<E>> {
+		
+		private double weight;
+		private E e;
+		private DijkstraNode<E> previous;
+		
+		public DijkstraNode(E e, double weight) {
+			this.weight = weight;
+			this.e = e;
+			this.previous = null;
+		}
+
+		@Override
+		public int compareTo(DijkstraNode<E> o) {
+			if (this.weight < o.weight)
+				return -1;
+			if (this.weight > o.weight)
+				return 1;
+			return 0;
 		}
 		
-		return null;
 	}
 	
 	
