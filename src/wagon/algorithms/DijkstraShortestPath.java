6ec5a8a2492d169f568ef0bfd5ddb1629600c050
Nemanja Milovanovic <nemanja@live.nl>
2016-04-15 16:49:47 +0200

    Fix and add some stuff.
    Added logging, fixed path construction and minor bugs.

--- src/wagon/algorithms/DijkstraShortestPath.java
+++ src/wagon/algorithms/DijkstraShortestPath.java
@@ -1,6 +1,7 @@
 package wagon.algorithms;
 
 import java.util.*;
+import java.util.logging.Logger;
 
 import wagon.network.Node;
 import wagon.network.WeightedEdge;
@@ -10,9 +11,15 @@ public class DijkstraShortestPath {
 
 	private EventActivityNetwork network;
 	
+	private Logger log = Logger.getLogger(this.getClass().getName());
+	
 	/**
 	 * Construct <code>DijkstraShortestPath</code> object for a given event-activity
 	 * network.
+	 * 
+	 * This class implements Dijkstra's shortest path algorithm using Java's own PriorityQueue. 
+	 * The algorithm that is used is one without decrease-key operations, where nodes are 
+	 * inserted into the heap only when necessary, thus saving memory.
 	 * 
 	 * @param network	event-activity network used to construct a shortest path
 	 */
@@ -20,7 +27,22 @@ public class DijkstraShortestPath {
 		this.network = network;
 	}
 	
+	/**
+	 * Constructs a single shortest path from <code>start</code> to <code>stop</code>.
+	 * 
+	 * It is entirely possible to perform multiple queries using this method.
+	 * 
+	 * @param start	start node
+	 * @param stop	stop node
+	 * @return	shortest path from <code>start</code> to <code>stop</code>
+	 */
 	public Path shortestPath(Node start, Node stop) {
+		
+		if (start == null || stop == null)
+			throw new IllegalArgumentException("Source and/or sink cannot be null");
+		
+		log.info("Commence shortest-path calculation...");
+		long startTime = System.nanoTime();
 		
 		Map<Node, Double> distance = new HashMap<>();
 		
@@ -37,8 +59,12 @@ public class DijkstraShortestPath {
 		
 		while (!queue.isEmpty()) {
 			DijkstraNode<Node> dijkU = queue.poll();
-			if (dijkU.e == stop)
+			if (dijkU.e == stop) {
+				long stopTime = System.nanoTime();
+				double duration = (stopTime-startTime)*1e-9;
+				log.info("...Finished calculating shortest path in: " + duration + " s");
 				return constructPath(dijkU);
+			}
 			double oldLabel = distance.get(dijkU.e);
 			if (dijkU.weight < oldLabel) {
 				distance.put(dijkU.e, dijkU.weight);
@@ -51,7 +77,7 @@ public class DijkstraShortestPath {
 				if (edge.source() != dijkU.e)
 					throw new IllegalStateException("Inconsistency detected");
 				if (dijkU.weight + edge.weight() < distance.get(v)) {
-					queue.add(new DijkstraNode<Node>(v, dijkU.weight + edge.weight()));
+					queue.add(new DijkstraNode<Node>(v, dijkU, dijkU.weight + edge.weight()));
 					distance.put(v, dijkU.weight + edge.weight());
 				}
 			}
@@ -61,11 +87,15 @@ public class DijkstraShortestPath {
 	
 	private Path constructPath(DijkstraNode<Node> end) {
 		DefaultPath path = new DefaultPath();
+		List<WeightedEdge> reversePath = new ArrayList<>();
 		DijkstraNode<Node> nextNode = end;
-		while (nextNode != null) {
-			path.addEdge(network.getEdge(nextNode.previous.e, nextNode.e));
+		while (nextNode.previous != null) {
+			reversePath.add(network.getEdge(nextNode.previous.e, nextNode.e));
 			nextNode = nextNode.previous;
 		}
+		Collections.reverse(reversePath);
+		for (WeightedEdge edge : reversePath)
+			path.addEdge(edge);
 		return path;
 	}
 	
@@ -76,9 +106,13 @@ public class DijkstraShortestPath {
 		private DijkstraNode<E> previous;
 		
 		public DijkstraNode(E e, double weight) {
+			this(e, null, weight);
+		}
+		
+		public DijkstraNode(E e, DijkstraNode<E> previous, double weight) {
 			this.weight = weight;
 			this.e = e;
-			this.previous = null;
+			this.previous = previous;
 		}
 
 		@Override
@@ -91,6 +125,4 @@ public class DijkstraShortestPath {
 		}
 		
 	}
-	
-	
 }
