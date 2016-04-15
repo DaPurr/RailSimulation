339cae0723408f11156393b3502b83186a3052f4
Nemanja Milovanovic <nemanja@live.nl>
2016-04-15 16:51:17 +0200

    Edit example.

--- src/wagon/Main.java
+++ src/wagon/Main.java
@@ -7,6 +7,7 @@ import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
 import wagon.algorithms.DijkstraShortestPath;
 import wagon.algorithms.Path;
+import wagon.network.Node;
 import wagon.network.expanded.EventActivityNetwork;
 import wagon.timetable.Timetable;
 
@@ -15,14 +16,14 @@ public class Main {
 	public static void main(String[] args) {
 //		EventActivityNetwork network = EventActivityNetwork.createTestNetwork();
 		try {
-			Timetable sample = Timetable.importFromExcel("data/smaller_sample_schedule1.xlsx");
+			Timetable sample = Timetable.importFromExcel("data/full_dataset.xlsx");
 			EventActivityNetwork network = EventActivityNetwork.createNetwork(sample);
 //			network.getStationDepartureNode("Rta", LocalDateTime.parse("2016-04-11T06:54"));
 			
 			DijkstraShortestPath dijkstra = new DijkstraShortestPath(network);
-			Path path = dijkstra.shortestPath(
-					network.getStationDepartureNode("Nwk", LocalDateTime.parse("2016-04-11T06:47")), 
-					network.getStationDepartureNode("Rtd", LocalDateTime.parse("2016-04-11T06:57")));
+			Node source = network.getStationDepartureNode("Vs", LocalDateTime.parse("2016-04-11T15:30"));
+			Node sink = network.getStationArrivalNode("Gn", LocalDateTime.parse("2016-04-11T20:46"));
+			Path path = dijkstra.shortestPath(source, sink);
 			System.out.println(path);
 //			System.out.println(network);
 		} catch (InvalidFormatException | IOException e) {
