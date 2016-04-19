11737cb478db2bd6d5ad9998de45e3f3a2af40a0
Nemanja Milovanovic <nemanja@live.nl>
2016-04-19 16:03:55 +0200

    Edit to compute all shortest paths.

--- src/wagon/algorithms/DijkstraShortestPath.java
+++ src/wagon/algorithms/DijkstraShortestPath.java
@@ -53,6 +53,7 @@ public class DijkstraShortestPath {
 			throw new IllegalStateException("Cannot find a suitable departure node");
 		
 		Map<Node, Double> distance = new HashMap<>();
+		Map<EventNode, DijkstraNode<EventNode>> eventToDijkstra = new HashMap<>();
 		
 		// init queue
 		PriorityQueue<DijkstraNode<EventNode>> queue = new PriorityQueue<>();
@@ -63,15 +64,32 @@ public class DijkstraShortestPath {
 		}
 
 		// insert source
-		queue.add(new DijkstraNode<EventNode>(start, 0.0));
+		DijkstraNode<EventNode> dijkSource = new DijkstraNode<EventNode>(start, 0.0);
+		eventToDijkstra.put(start, dijkSource);
+		queue.add(dijkSource);
+		double minimumCost = Double.POSITIVE_INFINITY;
 		
 		while (!queue.isEmpty()) {
 			DijkstraNode<EventNode> dijkU = queue.poll();
+			eventToDijkstra.put(dijkU.e, dijkU);
 			if ((dijkU.e instanceof ArrivalNode) && dijkU.e.trip().toStation().name().equals(to)) {
+				minimumCost = dijkU.weight;
+				List<DijkstraNode<EventNode>> endNodes = new ArrayList<>();
+				endNodes.add(dijkU);
+				for (DijkstraNode<EventNode> node : queue) {
+					if (node.weight <= minimumCost &&
+							(node.e instanceof ArrivalNode) &&
+							node.e.trip().toStation().name().equals(to)) {
+						endNodes.add(node);
+					}
+					else {
+						break;
+					}
+				}
 				long stopTime = System.nanoTime();
 				double duration = (stopTime-startTime)*1e-9;
 				log.info("...Finished calculating shortest path in: " + duration + " s");
-				return constructPath(dijkU);
+				return constructPath(endNodes);
 			}
 			double oldLabel = distance.get(dijkU.e);
 			if (dijkU.weight < oldLabel) {
@@ -84,40 +102,114 @@ public class DijkstraShortestPath {
 				EventNode v = edge.target();
 				if (edge.source() != dijkU.e)
 					throw new IllegalStateException("Inconsistency detected");
-				if (dijkU.weight + edge.weight() < distance.get(v)) {
-					queue.add(new DijkstraNode<EventNode>(v, dijkU, dijkU.weight + edge.weight()));
+				double distV = distance.get(v);
+				if (dijkU.weight + edge.weight() < distV) {
+					List<DijkstraNode<EventNode>> nodes = new ArrayList<>();
+					nodes.add(dijkU);
+					DijkstraNode<EventNode> newNode = new DijkstraNode<EventNode>(v, nodes, dijkU.weight + edge.weight());
+					queue.add(newNode);
 					distance.put(v, dijkU.weight + edge.weight());
+					eventToDijkstra.put(newNode.e, newNode);
+				} else if (dijkU.weight + edge.weight() == distV) {
+					DijkstraNode<EventNode> dijkV = eventToDijkstra.get(v);
+					if (dijkV == null)
+						throw new IllegalStateException("Could not find node v.");
+					dijkV.previous.add(dijkU);
 				}
 			}
 		}
 		throw new IllegalStateException("Could not find sink node(s)");
 	}
 	
-	private Path constructPath(DijkstraNode<EventNode> end) {
-		DefaultPath path = new DefaultPath();
-		List<WeightedEdge> reversePath = new ArrayList<>();
-		DijkstraNode<EventNode> nextNode = end;
-		while (nextNode.previous != null) {
-			reversePath.add(network.getEdge(nextNode.previous.e, nextNode.e));
-			nextNode = nextNode.previous;
+	private Path constructPath(List<DijkstraNode<EventNode>> endNodes) {
+		List<Path> paths = new ArrayList<>();
+		for (DijkstraNode<EventNode> node : endNodes) {
+			List<Path> dfsPaths = depthFirstSearch(node);
+			paths.addAll(dfsPaths);
 		}
-		Collections.reverse(reversePath);
-		for (WeightedEdge edge : reversePath)
-			path.addEdge(edge);
+		Set<Path> setPaths = new HashSet<>();
+		for (Path path : paths)
+			setPaths.add(path);
+		Path path = paths.get(0);
+		System.out.println(paths);
 		return path;
+	}
+	
+	// employ depth-first search to find all shortest paths
+	private List<Path> depthFirstSearch(DijkstraNode<EventNode> u) {
+//		List<Path> paths = new ArrayList<>();
+//		Stack<DijkstraNode<EventNode>> stack = new Stack<>();
+//		List<WeightedEdge> reversePath = new ArrayList<>();
+//		EventNode previous = u.e;
+//		for (DijkstraNode<EventNode> node : u.previous)
+//			stack.push(node);
+//		while (!stack.isEmpty()) {
+//			DijkstraNode<EventNode> v = stack.pop();
+//			EventNode current = v.e;
+//			WeightedEdge edge = network.getEdge(current, previous);
+//			if (edge == null)
+//				throw new IllegalStateException("Could not find edge (previous,current).");
+//			reversePath.add(edge);
+//			previous = v.e;
+//			if (v.previous == null) {
+//				DefaultPath path = new DefaultPath();
+//				Collections.reverse(reversePath);
+//				for (WeightedEdge e : reversePath)
+//					path.addEdge(e);
+//				paths.add(path);
+//				reversePath = new ArrayList<>();
+//				previous = u.e;
+//				continue;
+//			}
+//			for (DijkstraNode<EventNode> node : v.previous)
+//				stack.push(node);
+//		}
+//		return paths;
+		
+		List<Path> paths = new ArrayList<>();
+		Stack<List<DijkstraNode<EventNode>>> stack = new Stack<>();
+		
+		// initialize DFS
+		List<DijkstraNode<EventNode>> uList = new ArrayList<>();
+		uList.add(u);
+		stack.push(uList);
+		
+		while (!stack.isEmpty()) {
+			List<DijkstraNode<EventNode>> list = stack.pop();
+			List<DijkstraNode<EventNode>> neighbors = 
+					list.get(list.size()-1).previous;
+			if (neighbors == null) {
+				Collections.reverse(list);
+				DefaultPath path = new DefaultPath();
+				for (int i = 0; i < list.size()-1; i++) {
+					EventNode s = list.get(i).e;
+					EventNode t = list.get(i+1).e;
+					WeightedEdge edge = network.getEdge(s, t);
+					path.addEdge(edge);
+				}
+				paths.add(path);
+				continue;
+			}
+			for (DijkstraNode<EventNode> node : neighbors) {
+				List<DijkstraNode<EventNode>> listCopy = new ArrayList<>(list);
+				listCopy.add(node);
+				stack.push(listCopy);
+			}
+		}
+		return paths;
 	}
 	
 	private class DijkstraNode<E> implements Comparable<DijkstraNode<E>> {
 		
 		private double weight;
 		private E e;
-		private DijkstraNode<E> previous;
+		private List<DijkstraNode<E>> previous;
 		
 		public DijkstraNode(E e, double weight) {
 			this(e, null, weight);
 		}
 		
-		public DijkstraNode(E e, DijkstraNode<E> previous, double weight) {
+		public DijkstraNode(E e, List<DijkstraNode<E>> previous, double weight) {
 			this.weight = weight;
 			this.e = e;
 			this.previous = previous;
