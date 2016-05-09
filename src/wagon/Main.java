e8c1522d4d3a75ed97acf2d8234544ae7d7cdeb1
Nemanja Milovanovic <nemanja@live.nl>
2016-05-09 15:17:12 +0200

    Edit for earliest arrival return type change.

--- src/wagon/Main.java
+++ src/wagon/Main.java
@@ -2,10 +2,12 @@ package wagon;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 
 import wagon.algorithms.DijkstraShortestPath;
+import wagon.algorithms.JourneyGeneration;
 import wagon.algorithms.Path;
 import wagon.network.expanded.EventActivityNetwork;
 import wagon.timetable.Timetable;
@@ -17,18 +19,22 @@ public class Main {
 			Timetable sample = Timetable.importFromExcel("data/full_dataset.xlsx");
 			EventActivityNetwork network = EventActivityNetwork.createNetwork(sample);
 			DijkstraShortestPath dijkstra = new DijkstraShortestPath(network);
-			Path path1 = dijkstra.earliestArrivalPath("Vs", "Gn", LocalDateTime.parse("2016-04-11T15:30"));
+			List<Path> paths1 = dijkstra.earliestArrivalPath("Vs", "Gn", LocalDateTime.parse("2016-04-11T15:30"));
 			
 //			EventActivityNetwork network = EventActivityNetwork.createTestNetwork2();
 //			DijkstraShortestPath dijkstra = new DijkstraShortestPath(network);
-//			Path path2 = dijkstra.earliestArrivalPath("A", "C", LocalDateTime.parse("2016-04-19T10:50"));
+//			List<Path> paths2 = dijkstra.earliestArrivalPath("A", "C", LocalDateTime.parse("2016-04-19T10:50"));
 			
 //			EventActivityNetwork network = EventActivityNetwork.createTestNetwork3();
 //			DijkstraShortestPath dijkstra = new DijkstraShortestPath(network);
-//			Path path3 = dijkstra.earliestArrivalPath("A", "C", LocalDateTime.parse("2016-04-19T11:59"));
+//			List<Path> paths3 = dijkstra.earliestArrivalPath("A", "C", LocalDateTime.parse("2016-04-19T11:59"));
 			
-//			System.out.println(path1);
-//			System.out.println(path2);
+			JourneyGeneration jgen = new JourneyGeneration(network);
+			jgen.generateJourneys("Vs", "Gn", LocalDateTime.parse("2016-04-11T15:30"), 
+					LocalDateTime.parse("2016-04-11T20:17"));
+			
+//			System.out.println(paths1);
+//			System.out.println(paths2);
 		} catch (InvalidFormatException | IOException e) {
 			e.printStackTrace();
 		}
