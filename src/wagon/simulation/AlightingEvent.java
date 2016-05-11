4f1b185b2915da484d815fa07826996f58e080f0
Nemanja Milovanovic <nemanja@live.nl>
2016-05-11 15:40:12 +0200

    Add skeletons for several events.

---
+++ src/wagon/simulation/AlightingEvent.java
@@ -0,0 +1,13 @@
+package wagon.simulation;
+
+import java.time.LocalDateTime;
+
+public class AlightingEvent extends Event {
+
+	@Override
+	public LocalDateTime time() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/wagon/simulation/BoardingEvent.java
@@ -0,0 +1,13 @@
+package wagon.simulation;
+
+import java.time.LocalDateTime;
+
+public class BoardingEvent extends Event {
+
+	@Override
+	public LocalDateTime time() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
