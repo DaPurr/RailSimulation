6e51e0d6930b23b00fc278a7b84e9e57c95f8a7c
Nemanja Milovanovic <nemanja@live.nl>
2016-05-24 15:22:58 +0200

    Implement PassengerGroup class.

--- src/wagon/simulation/PassengerGroup.java
+++ src/wagon/simulation/PassengerGroup.java
@@ -1,5 +1,82 @@
 package wagon.simulation;
 
+import wagon.algorithms.DefaultPath;
+import wagon.algorithms.Path;
+
+/**
+ * This class represents a certain passenger group, where 
+ * passengers are aggregated into groups based on the <code>Path</code>s 
+ * that they take.
+ * 
+ * @author Nemanja Milovanovic
+ *
+ */
+
 public class PassengerGroup {
 
+	private DefaultPath path;
+	private double size;
+	
+	/**
+	 * Constructs a <code>PassengerGroup</code> object representing 
+	 * <code>path</code>, of size <code>size</code>.
+	 * 
+	 * @param path	the path that this group will take
+	 * @param size	group size
+	 */
+	public PassengerGroup(DefaultPath path, double size) {
+		this.path = path;
+		this.size = size;
+	}
+	
+	/**
+	 * Constructs a <code>PassengerGroup</code> object representing <code>path</code>, 
+	 * that is initially empty.
+	 * 
+	 * @param path	the path this group will take
+	 */
+	public PassengerGroup(DefaultPath path) {
+		this(path, 0.0);
+	}
+	
+	/**
+	 * Increments this group with 1.0.
+	 */
+	public void addPassenger() {
+		size += 1.0;
+	}
+	
+	/**
+	 * Sets the grop size <code>size</code> equal to <code>newSize</code>.
+	 * 
+	 * @param newSize
+	 */
+	public void setSize(double newSize) {
+		size = newSize;
+	}
+	
+	/**
+	 * @return group size
+	 */
+	public double size() {
+		return size;
+	}
+	
+	/**
+	 * @return	<code>Path</code> corresponding to this passenger 
+	 * 			group
+	 */
+	public Path getPath() {
+		return new DefaultPath(path);
+	}
+	
+	/**
+	 * Set the current <code>Path</code> for this passenger 
+	 * group.
+	 * 
+	 * @param path	<code>Path</code> to be traversed
+	 */
+	public void setPath(DefaultPath path) {
+		this.path = new DefaultPath(path);
+	}
 }
