85a81eb71a292365d847df4d674c3cf13c9e3128
Nemanja Milovanovic <nemanja@live.nl>
2016-06-01 23:02:52 +0200

    Add new tests.

--- src/wagon/Main.java
+++ src/wagon/Main.java
@@ -13,8 +13,10 @@ import wagon.algorithms.DijkstraShortestPath;
 import wagon.algorithms.RouteGeneration;
 import wagon.algorithms.RouteSelection;
 import wagon.algorithms.SLTLARouteSelection;
-import wagon.algorithms.Path;
+import wagon.algorithms.DefaultPath;
 import wagon.network.expanded.EventActivityNetwork;
+import wagon.simulation.Report;
+import wagon.simulation.SimModel;
 import wagon.simulation.SystemState;
 import wagon.timetable.Timetable;
 
@@ -22,26 +24,34 @@ public class Main {
 
 	public static void main(String[] args) {
 		try {
-//			Timetable sample = Timetable.importFromExcel("data/full_dataset.xlsx");
-//			Timetable sample = Timetable.importFromXML("data/processed/smaller_sample_schedule1_export.xml");
-			Timetable sample = Timetable.importFromXML("data/processed/full_dataset_export.xml");
-//			sample.export("data/processed/full_dataset_export.xml");
+//			Timetable sample = Timetable.importFromExcel("data/materieelplan/full_dataset.xlsx");
+			Timetable sample = Timetable.importFromExcel("data/materieelplan/smaller_sample_schedule1.xlsx");
+//			Timetable sample = Timetable.importFromXML("data/materieelplan/processed/smaller_sample_schedule1_export.xml");
+//			Timetable sample = Timetable.importFromXML("data/materieelplan/processed/full_dataset_export.xml");
+//			sample.export("data/materieelplan/processed/smaller_sample_schedule1_export.xml");
 			EventActivityNetwork network = EventActivityNetwork.createNetwork(sample);
-			
-//			DijkstraShortestPath dijkstra = new DijkstraShortestPath(network);
-//			List<Path> paths1 = dijkstra.earliestArrivalPath("Vs", "Gn", LocalDateTime.parse("2016-04-11T15:30"));
 			
 //			EventActivityNetwork network = EventActivityNetwork.createTestNetwork2();
 //			DijkstraShortestPath dijkstra = new DijkstraShortestPath(network);
-//			List<Path> paths2 = dijkstra.earliestArrivalPath("A", "C", LocalDateTime.parse("2016-04-19T10:50"));
+//			List<DefaultPath> paths1 = dijkstra.earliestArrivalPath("Nwk", "Rtd", LocalDateTime.parse("2016-04-11T06:45"));
+//			RouteSelection selectedLTLA1 = new SLTLARouteSelection(
+//					LocalDateTime.parse("2016-04-11T06:46"),
+//					LocalDateTime.parse("2016-04-11T06:58"),
+//					10);
+//			DefaultPath path8338930 = selectedLTLA1.selectPath(paths1);
+//			System.out.println(path8338930.representation());
+			
+//			EventActivityNetwork network = EventActivityNetwork.createTestNetwork2();
+//			DijkstraShortestPath dijkstra = new DijkstraShortestPath(network);
+//			List<DefaultPath> paths2 = dijkstra.earliestArrivalPath("A", "C", LocalDateTime.parse("2016-04-19T10:50"));
 			
 //			EventActivityNetwork network = EventActivityNetwork.createTestNetwork3();
 //			DijkstraShortestPath dijkstra = new DijkstraShortestPath(network);
-//			List<Path> paths3 = dijkstra.earliestArrivalPath("A", "C", LocalDateTime.parse("2016-04-19T11:59"));
+//			List<DefaultPath> paths3 = dijkstra.earliestArrivalPath("A", "C", LocalDateTime.parse("2016-04-19T11:59"));
 			
 //			long begin = System.nanoTime();
 //			RouteGeneration rgen = new RouteGeneration(network);
-//			List<Path> paths3 = rgen.generateRoutes(
+//			List<DefaultPath> paths3 = rgen.generateRoutes(
 //					"Vs", 
 //					"Gn", 
 //					LocalDateTime.parse("2016-04-11T15:30"), 
@@ -50,26 +60,26 @@ public class Main {
 //					LocalDateTime.parse("2016-04-11T15:30"),
 //					LocalDateTime.parse("2016-04-11T20:17"),
 //					10);
-//			Path path3 = selectedLTLA.selectPath(paths3);
+//			DefaultPath path3 = selectedLTLA.selectPath(paths3);
 //			long end = System.nanoTime();
 //			double duration = (end-begin)*1e-9;
 //			System.out.println("Time: " + duration);
 			
-			long begin = System.nanoTime();
-			RouteGeneration rgen = new RouteGeneration(network);
-			List<Path> paths4 = rgen.generateRoutes(
-					"Rta", 
-					"Dt", 
-					LocalDateTime.parse("2016-04-11T07:55"), 
-					LocalDateTime.parse("2016-04-11T08:33"));
-			RouteSelection selectedLTLA = new SLTLARouteSelection(
-					LocalDateTime.parse("2016-04-11T07:55"),
-					LocalDateTime.parse("2016-04-11T08:33"),
-					10);
-			Path path4 = selectedLTLA.selectPath(paths4);
-			long end = System.nanoTime();
-			double duration = (end-begin)*1e-9;
-			System.out.println("Time: " + duration);
+//			long begin = System.nanoTime();
+//			RouteGeneration rgen = new RouteGeneration(network);
+//			List<DefaultPath> paths4 = rgen.generateRoutes(
+//					"Rta", 
+//					"Dt", 
+//					LocalDateTime.parse("2016-04-11T07:55"), 
+//					LocalDateTime.parse("2016-04-11T08:33"));
+//			RouteSelection selectedLTLA = new SLTLARouteSelection(
+//					LocalDateTime.parse("2016-04-11T07:55"),
+//					LocalDateTime.parse("2016-04-11T08:33"),
+//					10);
+//			DefaultPath path4 = selectedLTLA.selectPath(paths4);
+//			long end = System.nanoTime();
+//			double duration = (end-begin)*1e-9;
+//			System.out.println("Time: " + duration);
 			
 //			System.out.println(paths1);
 //			System.out.println(paths2);
@@ -77,35 +87,39 @@ public class Main {
 //			System.out.println(paths4);
 					
 //			System.out.println(path3);
-			System.out.println(path4);
+//			System.out.println(path4);
 			
 //			String rep2 = paths2.get(0).representation();
 //			System.out.println(rep2);
-//			Path newPath2 = network.textToPath(rep2);
+//			DefaultPath newPath2 = network.textToPath(rep2);
 //			System.out.println(newPath2);
 			
-			String rep4 = path4.representation();
-			System.out.println(rep4);
-			Path newPath4 = network.textToPath(rep4);
-			System.out.println(newPath4);
-			System.out.println(newPath4.representation());
+//			String rep4 = path4.representation();
+//			System.out.println(rep4);
+//			DefaultPath newPath4 = network.textToPath(rep4);
+//			System.out.println(newPath4);
+//			System.out.println(newPath4.representation());
+			
+			SimModel sim = new SimModel(sample, network);
+			Report report = sim.start();
+			System.out.println(report.summary());
 			
 		}
 		catch (IOException e) {
 			e.printStackTrace();
 		} 
-		catch (SAXException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		catch (ParserConfigurationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} 
-//		catch (InvalidFormatException e) {
+//		catch (SAXException e) {
 //			// TODO Auto-generated catch block
 //			e.printStackTrace();
 //		}
+//		catch (ParserConfigurationException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		} 
+		catch (InvalidFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
