4ce34b1fd7decd6881084b8348bf86860a0a27f2
Nemanja Milovanovic <nemanja@live.nl>
2016-06-09 11:00:29 +0200

    Add some javadoc.

--- src/wagon/simulation/Options.java
+++ src/wagon/simulation/Options.java
@@ -1,27 +1,45 @@
 package wagon.simulation;
 
+/**
+ * This class is used to pass options to a <code>SimModel</code> object.
+ * 
+ * <p>
+ * As of now, <code>Option</code> contains only required options. It 
+ * is necessary to pass either a path to raw CiCo data, or to a file 
+ * containing passenger group data processed from raw CiCo data. Note 
+ * that the latter allows for much shorter computation times. Raw CiCo 
+ * data can be obtained through the <code>ExportPassengerGroups()</code> 
+ * method of the <code>SimModel</code> class. 
+ * 
+ * @author Nemanja Milovanovic
+ *
+ */
 public class Options {
 
 	private String pathToRawCiCoData;
 	private String pathToProcessedGroupsData;
 	private int dayOfWeek;
 	
-	public Options() {
-		pathToProcessedGroupsData = null;
-		pathToRawCiCoData = null;
-		dayOfWeek = 1;
-	}
-	
-	public void setDayOfWeek(int day) {
-		dayOfWeek = day;
-	}
-	
-	public void setpathToRawCiCoData(String fileName) {
-		pathToRawCiCoData = fileName;
-	}
-	
-	public void setPathToProcessedGroupsData(String fileName) {
-		pathToProcessedGroupsData = fileName;
+	/**
+	 * Constructs an object of class <code>Option</code>.
+	 * 
+	 * <p>
+	 * Note that that there is need of only one input file, so either 
+	 * <code>pathToRawCiCoData</code> or <code>pathToProcessedGroupsData</code> 
+	 * is expected to be <code>null</code>. If none of the arguments are, then 
+	 * the data regarding processed group data is used.
+	 * 
+	 * @param pathToRawCiCoData			the path to raw CiCo data
+	 * @param pathToProcessedGroupsData	the path to processed passenger group data
+	 * @param dayOfWeek					the day of the week
+	 */
+	public Options(
+			String pathToRawCiCoData,
+			String pathToProcessedGroupsData,
+			int dayOfWeek) {
+		this.pathToProcessedGroupsData = pathToProcessedGroupsData;
+		this.pathToRawCiCoData = pathToRawCiCoData;
+		this.dayOfWeek = dayOfWeek;
 	}
 	
 	public String getPathToRawCiCoData() {
