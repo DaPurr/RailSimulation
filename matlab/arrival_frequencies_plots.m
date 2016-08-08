8bb150e8c584b93789215cd4190f487aea702692
Nemanja Milovanovic <nemanja@live.nl>
2016-08-08 18:49:10 +0200

    Change plot script.

--- matlab/arrival_frequencies_plots.m
+++ matlab/arrival_frequencies_plots.m
@@ -1,15 +1,15 @@
 % 10 min interval
-%data_rta_20160112 = csvread('../data/cico/frequencies/20160112/freqs_rta_10min.csv');
+data_20160112 = csvread('../data/cico/export/20160112_rta.csv');
 %data_rta_ut_20160112 = csvread('../data/cico/frequencies/20160112/freqs_rta_ut_10min.csv');
 %data_rta_rtd_20160112 = csvread('../data/cico/frequencies/20160112/freqs_rta_rtd_10min.csv');
 %data_rta_gd_20160112 = csvread('../data/cico/frequencies/20160112/freqs_rta_gd_10min.csv');
 
-data_rta_20160209 = csvread('../data/cico/rta_20160209.csv');
-data_rta_ut_20160209 = csvread('../data/cico/rta_ut_20160209.csv');
+data_20160209 = csvread('../data/cico/export/20160209_rta.csv');
+% data_rta_ut_20160209 = csvread('../data/cico/rta_ut_20160209.csv');
 %data_rta_rtd_20160209 = csvread('../data/cico/rta_rtd_20160209.csv');
 %data_rta_gd_20160209 = csvread('../data/cico/frequencies/20160209/freqs_rta_gd_10min.csv');
 
-%data_rta_20160315 = csvread('../data/cico/frequencies/20160315/freqs_rta_10min.csv');
+data_20160315 = csvread('../data/cico/export/20160315_rta.csv');
 %data_rta_ut_20160315 = csvread('../data/cico/frequencies/20160315/freqs_rta_ut_10min.csv');
 %data_rta_rtd_20160315 = csvread('../data/cico/frequencies/20160315/freqs_rta_rtd_10min.csv');
 %data_rta_gd_20160315 = csvread('../data/cico/frequencies/20160315/freqs_rta_gd_10min.csv');
@@ -21,8 +21,20 @@ data_rta_ut_20160209 = csvread('../data/cico/rta_ut_20160209.csv');
 
 %x = 1:length(data_rta_20160209);
 %x_hf = 1:length(data_rta_rtd_hf);
-%figure;
-plot(data_rta_ut_20160209(:,1), data_rta_ut_20160209(:,2), 'Color', 'red');
+figure;
+hold on
+plot(data_20160112(:,1), data_20160112(:,2), 'Color', 'red');
+xlabel('t');
+ylabel('arrivals/t');
+figure;
+plot(data_20160209(:,1), data_20160209(:,2), 'Color', 'blue');
+xlabel('t');
+ylabel('arrivals/t');
+figure;
+plot(data_20160315(:,1), data_20160315(:,2), 'Color', 'magenta');
+xlabel('t');
+ylabel('arrivals/t');
+hold off
 %hold on;
 %plot(data_rta_20160209(:,1), data_rta_20160209(:,2), 'Color', 'red')
 %hold off;
---
+++ src/sandbox/CiCoDataSandbox.java
@@ -0,0 +1,40 @@
+package sandbox;
+
+import java.io.IOException;
+
+import wagon.data.CiCoData;
+import wagon.simulation.Options;
+
+public class CiCoDataSandbox {
+
+	public static void main(String[] args) {
+		Options options = new Options();
+		options.setDayOfWeek(2);
+		options.setPathToStations("data/cico/omzettabel_stations.csv");
+		options.setPathToCiCoData("data/cico/ritten_20160112.csv");
+		String station = "rta";
+		try {
+			CiCoData cicoData = CiCoData.importRawData(options);
+			cicoData.exportEmpiricalArrivalRateOfCheckInStation(
+					station, 
+					5*60, 
+					"data/cico/export/20160112_" + station + ".csv");
+			
+			options.setPathToCiCoData("data/cico/ritten_20160209.csv");
+			cicoData = CiCoData.importRawData(options);
+			cicoData.exportEmpiricalArrivalRateOfCheckInStation(
+					station, 
+					5*60, 
+					"data/cico/export/20160209_"+station+".csv");
+			
+			cicoData = CiCoData.importRawData(options);
+			cicoData.exportEmpiricalArrivalRateOfCheckInStation(
+					station, 
+					5*60, 
+					"data/cico/export/20160315_"+station+".csv");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
